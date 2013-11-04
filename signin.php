f3484e74b72672438b28f36758b105124d67baa8
Daniel Mendonça <asmendoncadaniel@gmail.com>
2013-11-04 15:21:05 +0000

    login and register files added
    ...

---
+++ signin.php
@@ -0,0 +1,34 @@
+<?php
+
+$email = $_GET['email'];
+$password = $_GET['password'];
+
+
+$db = new PDO('sqlite:database.sqlite');
+$stmt="SELECT * FROM Costumer WHERE Email=? AND Password=?;";
+
+$email= "admin@ois.com";
+$password= "admin";
+
+$query=$db->prepare($stmt);
+$query->bindParam(1,$email);
+$query->bindParam(2,$password);
+
+$result=$query->execute();
+$result=$result->fetchAll();
+
+if(!$result){
+	?>
+	<script type="text/javascript">
+		function(){
+			alert("Wrong User and/or Password");
+		}
+	</script>
+	<?php
+}
+
+else{
+	 session_start();
+	   $_SESSION['customer'] = $email;
+	   $_SESSION['pwd'] = $password;
+}
---
+++ signupform.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Sign up</title>
+	</head>
+
+	<body>
+		<form action="signup.php" method="GET">
+			Name:<br>
+			<input type="text" value="name" name="name"><br>
+			Tax number:<br>
+			<input type="text" value="customer tax id" name="cTaxID"><br>
+			E-mail address:<br>
+			<input type="email" value="email" name="email"><br>
+			Password:<br>
+			<input type="password" value="password" name="password"><br>
+			Country:<br>
+			<input type="text" value="country" name="country"><br>
+			Address:<br>
+			<input type="text" value="address" name="addr"><br>
+			City:<br>
+			<input type="text" value="city" name="city"><br>
+			Postal-Code:<br>
+			<input type="text" value="Postal Code" name="postalC1"> - <input type="text" value="Postal Code 2" name="postalC2"><br>
+			<input type="submit" value="Sign In">
+			<input type="button" value="Sign Up"/>
+		</form>
+	</body>
+
+</html>
