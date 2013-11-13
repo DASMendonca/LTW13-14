e7886d3b187f0c56c06b99f143d820c9008b8301
Jose Moreira <jpmoreiradev@gmail.com>
2013-11-13 16:21:34 +0000

    Implementation of some functions to be used in the api

--- classesTest.php
+++ classesTest.php
@@ -2,10 +2,15 @@
 <?php
 include 'classes.php';
 
-$db = new PDO('sqlite:database.sqlite');
-$newTax= new Tax(null);
+$db = new PDO('sqlite:./database.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$newTax->fetchFromDB($db, 1);
-echo $newTax->taxID;
-echo $newTax->value;
+$params=array(array("TaxValue",13),array("TaxID",2));
+$query=constructSelect("Tax", $params,$db);
+//$query=$db->prepare("Select * from Tax WHERE  TaxValue = ? AND TaxID = ?;");
+
+
+$res=$query->execute();
+$result=$query->fetchAll();
 ?>
