3e1d680d9c38ab165c90971b36663a7f89b5459a
Jose Moreira <jpmoreiradev@gmail.com>
2013-11-14 12:42:08 +0000

    Implemented part of Costumers and Products API
    Errors still not working properly

--- api/searchCustomersByField.php
+++ api/searchCustomersByField.php
@@ -1,9 +1,27 @@
 <?php
 
-//TODO set header type
-include 'classes.php';
+header('Content-type: application/json');
+include '../classes.php';
 
-$customers=Customer::getInstancesByFields($db, $_GET);
+$db = new PDO('sqlite:../database.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-echo json_encode($customers);
+$params=array(
+	array($_GET["field"],$_GET["value"],$_GET["op"])
+);
+
+
+$customers=array();	
+try {
+	$customers=Customer::getInstancesByFields($db, $params);
+	echo json_encode($customers);
+} catch (ApiException $e) {
+	echo json_encode($e);
+}
+
+
+
+
+
 ?>
--- classesTest.php
+++ classesTest.php
@@ -7,9 +7,8 @@ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 $parameters=array(
-	array("TaxID",array(0,3),"range"),
-	array("TaxValue",array(13),"equal"),
+	array("City",array("Porto"),"equal")
 );
 
-$result=Tax::getInstancesByFields($db, $parameters);
+$result=Address::getInstancesByFields($db, $parameters);
 ?>
