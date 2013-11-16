72af2c78b67a2a06db39a08eb91558b1a76c6b1b
Jose Moreira <jpmoreiradev@gmail.com>
2013-11-16 13:44:16 +0000

    Product private api

---
+++ private_api/getProduct.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$db = new PDO('sqlite:./database.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+$parameters=array(
+	array("City",array("Porto"),"equal")
+);
+
+try {
+	$products=Product::getInstancesByFields($db, $_POST);
+	echo json_encode($products);
+} catch (ApiException $e) {
+	echo json_encode($e);
+}
+
+
+?>
