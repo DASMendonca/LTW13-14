7a30503e6eb8cdc295bf03bf23863260c730ae83
Daniel Mendonça <ei12167@fe.up.pt>
2013-11-16 13:37:40 +0000

    resolve

--- classesTest.php
+++ classesTest.php
@@ -2,20 +2,5 @@
 <?php
 include 'classes.php';
 
-$db = new PDO('sqlite:./database.sqlite');
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-$parameters=array(
-<<<<<<< HEAD
-	array("ProductCode",array(2),"equal"),
-);
-
-$result=Product::getInstancesByFields($db, $parameters);
-=======
-	array("City",array("Porto"),"equal")
-);
-
-$result=Address::getInstancesByFields($db, $parameters);
->>>>>>> 354d9c031d6bdadd7ecc563d9861b5ac6847c400
 ?>
