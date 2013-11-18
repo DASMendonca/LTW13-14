baf1e5d9df050c746e5dfba8ea801d0e52d31842
vmineiro <vmineiro@users.noreply.github.com>
2013-11-18 11:02:19 +0000

    detailinvoice updated

--- DetailInvoice.php
+++ DetailInvoice.php
@@ -8,65 +8,225 @@
 </head>
 
 <body>
-<table class="Logo">
-<tr>
-<th>Sistema de Faturação Online</th>
-</tr>
-<tr>
-<td>Linguagens e Tecnologias Web</td>
-</tr>
-</table>
-<br>
+	<table class="Logo">
+		<tr>
+			<th>Sistema de Faturação Online</th>
+		</tr>
+		<tr>
+			<td>Linguagens e Tecnologias Web</td>
+		</tr>
+	</table>
 
-<p class="sheetID">Dados de Cliente</p><br><br>
-<p class="rowID">Código de Cliente: </p>
-<p>0001 </p><br>
-<p class="rowID">Nome: </p>
-<p> Filomena</p><br>
-<p class="rowID">NIF: </p>
-<p>123456321</p><br>
-<br>
+	<p class="sheetID">Dados de Cliente</p>
+	<br>
+	<br>
+	<p class="rowID">Código de Cliente:</p>
+	<p>0001</p>
+	<br>
+	<p class="rowID">Nome:</p>
+	<p>Filomena</p>
+	<br>
+	<p class="rowID">NIF:</p>
+	<p>123456321</p>
+	<br>
+	<br>
 
-<p class="sheetID">Factura</p><br>
-<table class="invoice">
-<tr>
-<td class="rowID">Fatura n.º: </td>
-<td>0001 </td>
-<td class="rowID">Data: </td>
-<td> 2013-12-12</td>
-</tr>
-</table><br>
+	<p class="sheetID">Factura</p>
+	<br>
+	<table class="invoice">
+		<tr>
+			<td class="rowID">Fatura n.º:</td>
+			<td>0001</td>
+			<td class="rowID">Data:</td>
+			<td>2013-12-12</td>
+		</tr>
+	</table>
+	<br>
 
-<p class="Articles">Artigos</p><br>
-<table class="products">
-<tr  class="rowID">
-<th>Cód. Produto</th>
-<th>Descrição do Produto</th>
-<th>UN</th>
-<th>Quantidade</th>
-<th>Preço Unit.</th>
-<th>IVA</th>
-<th>Preço Total</th>
-</tr>
-<tr class="productRow">
-<td class="ProdCode">00001</td>
-<td class="ProDes">Produto 1</td>
-<td class="UN">un</td>
-<td class="QTT">1.00</td>
-<td class="UnitPri">100.00</td>
-<td class="IVA">23</td>
-<td class="TotPri">123.00</td>
-</tr>
-<tr class="productRow">
-<td class="ProdCode">00001</td>
-<td class="ProDes">Produto 1</td>
-<td class="UN">un</td>
-<td class="QTT">1.00</td>
-<td class="UnitPri">100.00</td>
-<td class="IVA">23</td>
-<td class="TotPri">123.00</td>
-</tr>
-</table>
+	<p class="Articles">Artigos</p>
+	<br>
+	<table class="products">
+		<tr>
+			<th>Cód. Produto</th>
+			<th>Descrição do Produto</th>
+			<th>UN</th>
+			<th>Quantidade</th>
+			<th>Preço Unit.</th>
+			<th>IVA</th>
+			<th>Preço Total</th>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+		<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+				<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+		<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+		<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+		<tr>
+			<td>00001</td>
+			<td>Produto 1</td>
+			<td>un</td>
+			<td>1.00</td>
+			<td>100.00</td>
+			<td>23</td>
+			<td>123.00</td>
+		</tr>
+	</table>
 
 
 </body>
