b7248a30d876c41e9d0fd9964ab71c795361608b
Jose Moreira <ei12002@fe.up.pt>
2013-12-03 10:40:26 +0000

    Update Customers implemented

--- private_api/changeCustomer.php
+++ private_api/changeCustomer.php
@@ -3,9 +3,12 @@
 include 'classes.php';
 
 header('Content-type: application/json');
-
+$db = new PDO('sqlite:../database.sqlite');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 try {
+	
 	
 	
 	if(!isset($POST["parameters"]) || count($POST["parameters"])==0 ) throw new GeneralException(new Err_MissingParameter("parameters"));
@@ -15,7 +18,7 @@ try {
 	
 	
 	$id=$parameters[1][0];
-	if($id="") 
+	if($id="") json_encode(Customer::updateInDB($db, $parameters));
 	
 	
 	
