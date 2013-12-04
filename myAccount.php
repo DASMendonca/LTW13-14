03bb4471de67a1d819e80380254dc1a3020c0565
Daniel Mendonça <ei12167@fe.up.pt>
2013-12-04 17:09:41 +0000

    Reolved more issues

--- myAccount.php
+++ myAccount.php
@@ -23,7 +23,7 @@ echo '
 
 				<div id="Email">
 				<label class="to_ident" for="email">E-mail</label>
-				<input type="text" name="email" id="email" placeholder="'.$_SESSION['customer']->email.'" value="'.$_SESSION['customer']->email.'"><br>
+				<input type="text" name="email" id="email" placeholder="'.$_SESSION['customer']->Email.'" value="'.$_SESSION['customer']->Email.'"><br>
 				</div>
 						
 				<div id="CustomerTaxID">
@@ -33,30 +33,30 @@ echo '
 
 				<div id="Password">
 				<label class="to_ident" for="password">Password</label>
-				<input type="text" name="password" id="password" placeholder="'.$_SESSION['customer']->password.'" value="'.$_SESSION['customer']->password.'"><br>
+				<input type="text" name="password" id="password" placeholder="'.$_SESSION['customer']->Password.'" value="'.$_SESSION['customer']->Password.'"><br>
 				</div>
 						
 				<div id="Country">
 				<label class="to_ident" for="Country">Country</label>
-				<input type="text" name="Country" id="Country" placeholder="'.$_SESSION['customer']->getAddress()->country.'" value="'.$_SESSION['customer']->getAddress()->country.'"><br>
+				<input type="text" name="Country" id="Country" placeholder="'.$_SESSION['customer']->getAddress()->Country.'" value="'.$_SESSION['customer']->getAddress()->Country.'"><br>
 				</div>
 				
 				<div id="City">
 				<label class="to_ident" for="City">City</label>
-				<input type="text" name="City" id="City" placeholder="'.$_SESSION['customer']->getAddress()->city.'" value="'.$_SESSION['customer']->getAddress()->city.'"><br>
+				<input type="text" name="City" id="City" placeholder="'.$_SESSION['customer']->getAddress()->City.'" value="'.$_SESSION['customer']->getAddress()->City.'"><br>
 				</div>
 						
 				<div id="AddressDetail">
 				<label class="to_ident" for="AddressDetail"></label>
-				<input type="text" name="AddressDetail" id="AddressDetail" placeholder="'.$_SESSION['customer']->getAddress()->detail.'" value="'.$_SESSION['customer']->getAddress()->detail.'"><br>
+				<input type="text" name="AddressDetail" id="AddressDetail" placeholder="'.$_SESSION['customer']->getAddress()->AddressDetail.'" value="'.$_SESSION['customer']->getAddress()->AddressDetail.'"><br>
 				</div>
 					
 				<div id="PostalCode1">
 				<label class="to_ident" for="PostalCode1"></label>
-				<input type="text" name="PostalCode1" id="PostalCode1" placeholder="'.$_SESSION['customer']->getAddress()->postalCode1.'" value="'.$_SESSION['customer']->getAddress()->postalCode1.'">-
+				<input type="text" name="PostalCode1" id="PostalCode1" placeholder="'.$_SESSION['customer']->getAddress()->PostalCode1.'" value="'.$_SESSION['customer']->getAddress()->PostalCode1.'">-
 				</div>
 				<div id ="PostalCode2">
-				<input type="text" name="PostalCode2" id="PostalCode2" placeholder="'.$_SESSION['customer']->getAddress()->postalCode2.'" value="'.$_SESSION['customer']->getAddress()->postalCode2.'"><br>
+				<input type="text" name="PostalCode2" id="PostalCode2" placeholder="'.$_SESSION['customer']->getAddress()->PostalCode2.'" value="'.$_SESSION['customer']->getAddress()->PostalCode2.'"><br>
 				</div>
 				<input type="button" id="save_edit" value="Save">				
 			</fieldset>
