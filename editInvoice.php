c67617fa15d52d99f2d20d72d4b1fde74aa54f11
Daniel Mendonça <ei12167@fe.up.pt>
2013-12-06 19:53:58 +0000

    changes

---
+++ editInvoice.php
@@ -0,0 +1,94 @@
+<?php
+include './classes.php';
+session_start();
+header('Content-type: text/html');
+
+
+if(!isset($_SESSION['customer']) || $_SESSION['customer']->Permission<2) 
+	header("Location: onlineInvoiceSystem.php");
+	
+
+	$db = new PDO('sqlite:./database.sqlite');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+	
+	$params=array(
+			array("InvoiceNO",array($_GET["param"]),"equal")
+	);
+	
+
+	$invoices = Invoice::getInstancesByFields($db, $params);
+	$invoice= $invoices[0];
+	$lines= $invoice->getLines();
+	
+		
+	/********************************************
+	 * 				Invoice Fields				*
+	 * 											*
+	 * 1- $invoice->getLines()					*
+	 * 2- $invoice->getCustomerId()				*
+	 * 3- $invoice->CompanyName					*
+	 * 4- $invoice->GrossTotal					*
+	 * 5- $invoice->InvoiceDate					*
+	 * 6- $invoice->InvoiceNo					*
+	 * 											*
+	 ********************************************/
+	$params=array(
+		array("CustomerID", array($invoice->getCustomerId()), "equal")
+	);
+	
+	$customer= Customer::getInstancesByFields($db, $params);
+	 
+	 echo '
+<div class="update_div" name="Invoice" id="false">
+		<br><br>
+		<form action="updateMyInvoice.php" method="post" class="update_form" name="InvoiceNo" id="'.$invoice->InvoiceNo.'">
+			<fieldset>
+				<legend><h2>Edit Information</h2></legend>
+				<div>
+				<label name="CompanyName" value="'.$invoice->CompanyName.'"></label>
+				</div><br>
+
+				<div>
+				<label class="to_ident" value="'.$invoice->InvoiceDate.'">
+				</div><br>
+						
+				<div id="CustomerTaxID">
+				<label class="to_ident" for="CustomerTaxID">Tax ID</label>
+				<input type="number" name="CustomerTaxID" id="CustomerTaxID" placeholder="'.$customer->CustomerTaxID.'" value="'.$customer->CustomerTaxID.'"><br>
+				</div>		
+
+				<div id="Password">
+				<label class="to_ident" for="password">Password</label>
+				<input type="text" name="password" id="password" placeholder="'.$customer->Password.'" value="'.$customer->Password.'"><br>
+				</div>
+						
+				<div id="Country">
+				<label class="to_ident" for="Country">Country</label>
+				<input type="text" name="Country" id="Country" placeholder="'.$customer->getAddress()->Country.'" value="'.$customer->getAddress()->Country.'"><br>
+				</div>
+				
+				<div id="City">
+				<label class="to_ident" for="City"></label>
+				<input type="text" name="City" id="City" placeholder="'.$customer->getAddress()->City.'" value="'.$customer->getAddress()->City.'"><br>
+				</div>
+						
+				<div id="AddressDetail">
+				<label class="to_ident" for="AddressDetail"></label>
+				<input type="text" name="AddressDetail" id="AddressDetail" placeholder="'.$customer->getAddress()->AddressDetail.'" value="'.$customer->getAddress()->AddressDetail.'"><br>
+				</div>
+					
+				<div id="PostalCode1">
+				<label class="to_ident" for="PostalCode1"></label>
+				<input type="text" name="PostalCode1" id="PostalCode1" placeholder="'.$customer->getAddress()->PostalCode1.'" value="'.$customer->getAddress()->PostalCode1.'">-
+				</div>
+				<div id ="PostalCode2">
+				<input type="text" name="PostalCode2" id="PostalCode2" placeholder="'.$customer->getAddress()->PostalCode2.'" value="'.$customer->getAddress()->PostalCode2.'"><br>
+				</div>
+				<input type="button" id="save_edit" value="Save">				
+			</fieldset>
+		</form>
+</div>';
+	
+?>
--- searchInvoice.php
+++ searchInvoice.php
@@ -4,16 +4,16 @@
 <form id="search_form" class="bySearch" name="Invoice" method="GET">
 	<h1>Invoice Search:</h1>
 	<div id="InvoiceNo">
-		<label>Invoice number:</label><input type="number" name="InvoiceNo" class="inputfield" placeholder="Invoice Number">
+		<label>Invoice number:</label><input id="InvoiceNumber" type="number" name="InvoiceNo" class="inputfield" placeholder="Invoice Number">
 	</div>
 	<div id="CustomerID">
-		<label>Customer ID:</label><input type="number" name="CustomerID" class="inputfield" placeholder="Customer Identification">
+		<label>Customer ID:</label><input id="CustomerID" type="number" name="CustomerID" class="inputfield" placeholder="Customer Identification">
 	</div>
 	<div id="CompanyName">
-		<label>Company Name:</label><input type="text" name="CompanyName" class="inputfield" placeholder="Company Name">
+		<label>Company Name:</label><input id="CompanyName" type="text" name="CompanyName" class="inputfield" placeholder="Company Name">
 	</div>
 	<div id="InvoiceDate">
-		<input type="date" name="InvoiceDate" class="inputfield" placeholder="YYYY-MM-DD">
+		<label>Date of emission</label><input id="invoiceDate" type="date" name="InvoiceDate" class="inputfield" placeholder="YYYY-MM-DD">
 	</div>
 	<input type="button" value="search" id="search_button">
 </form>
